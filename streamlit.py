5a96d36f8dba8ab112aa7838f51efc9d0463d720
quanweilei <quanweilei@gmail.com>
2022-08-17 12:53:14 -0700

    nah

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,9 @@
 import streamlit as st
 import pandas as pd
 
-while True:
-    st.title("Video Game Sales Dataset")
-    st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
-    df = pd.read_csv("vgsales.csv", index_col = 0)
-    table = st.radio('Display Table', ["yes", "no"])
-    while table == 'yes':
-        st.dataframe(df, 100000, 500)
+st.title("Video Game Sales Dataset")
+st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
+df = pd.read_csv("vgsales.csv", index_col = 0)
+table = st.radio('Display Table', ["yes", "no"])
+if table == 'yes':
+    st.dataframe(df, 100000, 500)
