2a379bdbc15d76bb8be27d656b140514110f30a9
quanweilei <quanweilei@gmail.com>
2022-08-17 12:11:56 -0700

    queens

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title("Queen Problem")
-size = st.select_slider('Board Size', options = [4, '10'])
+board = [4,5,6,7,8,9,10]
+queens = [1,2,3,4,5,6,7,8,9,10]
+size = st.select_slider('Board Size', options = board)
 input = st.select_slider('Number of Queens', options = [1, '10'])
 
 
