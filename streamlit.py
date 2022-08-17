f1536ad53569082d3a7c04eea268004d6566e217
quanweilei <quanweilei@gmail.com>
2022-08-17 12:01:04 -0700

    scratched

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Calculator Test App")
-option = st.selectbox('Select One', ('Addition', 'Subtraction', 'Multiplication', 'Division'))
-st.write('You selected', option)
+st.code("code code code code")
+
