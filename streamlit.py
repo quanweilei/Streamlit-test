c3f1ca361e2cce9aaf1f32bb542b253364184496
quanweilei <quanweilei@gmail.com>
2022-08-17 11:40:12 -0700

    calc

--- streamlit.py
+++ streamlit.py
@@ -1,4 +1,6 @@
 import streamlit as st
 import pandas as pd
 
-st.title("TEST APP TITLE")
+st.title("Calculator Test App")
+option = st.selectbox('Select One', 'Addition', 'Subtraction', 'Multiplication', 'Division')
+st.write('You selected', option)
