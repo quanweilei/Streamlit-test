fe713b0fad4b929d63647800a69a6a35b05a9c48
quanweilei <quanweilei@gmail.com>
2022-08-17 12:36:59 -0700

    csv

--- streamlit.py
+++ streamlit.py
@@ -4,5 +4,6 @@ import pandas as pd
 
 st.title("Video Game Sales Dataset")
 st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
-df = pd.read_csv("vgsales.csv")
+url = "https://github.com/quanweilei/Streamlit-test/blob/bd803a68a546c1c02f51f0a08fa8a2fddfdead11/vgsales.csv"
+df = pd.read_csv(url, index_col = 0)
 st.dataframe(df)
