e09487d1286c8dc7552af72ac546636ed29fa3c2
quanweilei <quanweilei@gmail.com>
2022-08-17 12:58:45 -0700

    added locating

--- streamlit.py
+++ streamlit.py
@@ -5,5 +5,10 @@ st.title("Video Game Sales Dataset")
 st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
 df = pd.read_csv("vgsales.csv", index_col = 0)
 table = st.radio('Display Table', ["yes", "no"])
+input = st.text_input("Search for Game")
+if df['Name'].__contains__(input) == False:
+    st.write("Game Does not Exist")
+else:
+    st.table(df.loc[df['Name'] == input])
 if table == 'yes':
     st.dataframe(df, 100000, 500)
