02d61bed43cb9113e24d28e8f76af4521ffb7e89
quanweilei <quanweilei@gmail.com>
2022-08-17 12:24:16 -0700

    minecraft

--- streamlit.py
+++ streamlit.py
@@ -1,10 +1,5 @@
 import streamlit as st
 import pandas as pd
 
-st.title("Queen Problem")
-board = [4,5,6,7,8,9,10]
-queens = [1,2,3,4,5,6,7,8,9,10]
-size = st.select_slider('Board Size', options = board)
-input = st.select_slider('Number of Queens', options = queens)
-
-
+st.title("Video Game Sales Dataset")
+st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
