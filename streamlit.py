beed08e08491eeb865f88f217a1d1caca7dd4dc6
quanweilei <quanweilei@gmail.com>
2022-08-17 12:43:37 -0700

    stream

--- streamlit.py
+++ streamlit.py
@@ -5,4 +5,4 @@ import pandas as pd
 st.title("Video Game Sales Dataset")
 st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
 df = pd.read_csv("vgsales.csv", index_col = 0)
-st.dataframe(df, 100, 100)
+st.dataframe(df, 100000, 1000)
