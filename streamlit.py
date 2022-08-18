bb6bc59411dc08ed541805d4ad17e3a62509ca1b
quanweilei <quanweilei@gmail.com>
2022-08-18 10:47:34 -0700

    update

--- streamlit.py
+++ streamlit.py
@@ -4,12 +4,14 @@ import pandas as pd
 st.title("Video Game Sales Dataset")
 st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
 df = pd.read_csv("vgsales.csv", index_col = 0)
+sf = df
+
+filter = st.text_input("Filter by Platform")
 
 input = st.text_input("Search for Game")
-
 if df['Name'].isin([input]).empty == False:
-    st.table(df.loc[df['Name'] == input])
+    st.table(df.loc[df['Name'].str.contains(input, case = False)])
 
 table = st.radio('Display Table', ["yes", "no"])
 if table == 'yes':
-    st.dataframe(df, 100000, 500)
+    st.dataframe(sf, 100000, 500)
