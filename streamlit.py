80b03d543392e620ce1cda1ab0417278733ac43e
quanweilei <quanweilei@gmail.com>
2022-08-18 12:01:57 -0700

    hahahah

--- streamlit.py
+++ streamlit.py
@@ -13,8 +13,15 @@ if df['Platform'].isin([input]).empty == False and len(filter) != 0:
 
 input = st.text_input("Search for Game")
 if df['Name'].isin([input]).empty == False and len(input) != 0:
+    filter = st.text_input("Filter by Platform")
+    if df['Platform'].isin([input]).empty == False and len(filter) != 0:
+        df = df.loc[df['Platform'] == filter]
+    df.reset_index(drop = True)
     st.write("Currently filtering for ", filter)
     st.table(df.loc[df['Name'].str.contains(input, case = False)])
+
+
+
 
 table = st.radio('Display Table', ["yes", "no"])
 if table == 'yes':
