c4f0debf8dbe755994e3b93e6c93443b4da8c8f1
quanweilei <quanweilei@gmail.com>
2022-08-24 13:24:24 -0700

    commit

--- streamlit.py
+++ streamlit.py
@@ -4,10 +4,13 @@ import pandas as pd
 st.title("Video Game Sales Dataset")
 st.image("https://www.minecraft.net/content/dam/games/minecraft/marketplace/mediablock-buzzybees.jpg")
 df = pd.read_csv("vgsales.csv", index_col = 0)
+
 sf = df
 
+
+
 input = st.text_input("Search for Game")
-if (df.loc[df['Name'].str.contains(input, na = False)]).empty == False and len(input) != 0:
+if df['Name'].isin([input]).empty == False and len(input) != 0:
     option = st.selectbox('Select Game', (df.loc[df['Name'].str.contains(input, case = False)]))
     #filter = st.selectbox("Filter by Platform", (df.loc[df['Platform'] == filter]))
     #df = df.loc[df['Platform'] == filter]
